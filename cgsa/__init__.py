ea3a3c84aed20bfed3b3591b420bb6a6d074d67b
Wladimir Sidorenko <wlsidorenko@gmail.com>
2017-07-17 08:10:12 +0200

    added initial module file

---
+++ cgsa/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+
+##################################################################
+# Documentation
+"""Main meta-package containing a collection of discourse segmenters.
+
+Attributes:
+  lexicon (module):
+    routines common to multiple subpackages
+  dl (subpackage):
+    rule-based discourse segmenter for Mate dependency trees
+  ml (subpackage):
+    auxiliary segmenter routines used by syntax-driven segmenters
+  evaluation (subpackage):
+    metrics for evaluating discourse segmentation
+
+.. moduleauthor:: Wladimir Sidorenko (Uladzimir Sidarenka)
+
+"""
+
+##################################################################
+# Imports
+
+##################################################################
+# Variables and Constants
+__name__ = "cgsa"
+__all__ = ["lexicon", "dl", "ml", "evaluation"]
+__author__ = "Uladzimir Sidarenka"
+__email__ = "sidarenk at uni dash potsdam dot de"
+__version__ = "0.1.0a0"
