947a52ffdfbe54c39b1a7b69a60d4d0b4b247aa2
Wladimir Sidorenko <wlsidorenko@gmail.com>
2017-07-17 08:10:27 +0200

    added constants file

---
+++ cgsa/constants.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+
+##################################################################
+# Documentation
+"""Collection of variables and constants common to all modules.
+
+Attributes:
+  DFLT_MODEL_PATH (str): path to the main default model file
+
+"""
+
+##################################################################
+# Imports
+from __future__ import absolute_import
+
+import os
+
+
+##################################################################
+# Variables and Constants
+ENCODING = "utf-8"
+
+DIRNAME = os.path.dirname(__file__)
+DATA_DIR = os.path.join(DIRNAME, "data")
+
+MODEL_DIR = os.path.join(DATA_DIR, "models")
+DFLT_MODEL_PATH = os.path.join(MODEL_DIR, "pdtb.sense.model")
+
+TABOADA = "taboada"
+
+# labels
+POSITIVE = "positive"
+NEGATIVE = "negative"
+UNKNOWN = "unknown"
+NEUTRAL = "neutral"
+MIXED = "mixed"
+KNOWN_LABELS = set([POSITIVE, NEGATIVE, UNKNOWN, NEUTRAL, MIXED])
