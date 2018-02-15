90fe1eaf414b68513c8485a2f1af7b4321a057a3
Wladimir Sidorenko <wlsidorenko@gmail.com>
2018-02-15 03:05:18 +0100

    default vector dimensionality set to 35 according to Sochers implementation

--- cgsa/dl/rnn.py
+++ cgsa/dl/rnn.py
@@ -26,7 +26,7 @@ class RNNAnalyzer(TreeRNNBaseAnalyzer):
     def __init__(self, *args, **kwargs):
         super(RNNAnalyzer, self).__init__(*args, **kwargs)
         self.name = "rnn"
-        self.ndim = 5
+        self.ndim = 35
 
     def _init_rnn(self, inputs):
         """Method defining a recurrent unit.
