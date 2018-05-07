1acd3d6b5e2774674212513f201f98510726c387
Wladimir Sidorenko <wlsidorenko@gmail.com>
2018-05-07 11:28:34 +0200

    added __init__ file to the cgsa utils module

---
+++ cgsa/utils/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+
+##################################################################
+# Documentation
+"""Package containing common utility functions.
+
+.. moduleauthor:: Wladimir Sidorenko (Uladzimir Sidarenka)
+
+"""
+
+##################################################################
+# Imports
+from __future__ import absolute_import, print_function, unicode_literals
+
+##################################################################
+# Variables and Constants
+__name__ = "cgsa.utils"
+__author__ = "Uladzimir Sidarenka"
+__email__ = "sidarenk at uni dash potsdam dot de"
+__version__ = "0.0.0"
